7740686dff38a28d018e1b655030a8ae6dcef4e0
Muhammad Zaid Dildar <142285065+Zaid-Dildar@users.noreply.github.com>
2024-11-07 01:55:42 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("height updated");
+console.log("height updated from external script");
 function isMobileScreen() {
 return window.innerWidth <= 768; // Adjusts breakpoint for mobile
 }
@@ -11,6 +11,8 @@ setIframeHeight();
 
 window.addEventListener("message", function (event) {
 if (event.data.type === "resize-iframe") {
+console.log("Inside event listener");
+console.log(event);
 document.getElementById("iframe").style.height =
 event.data.height[isMobileScreen() ? 1 : 0];
 console.log("height updated in hostinger script");
