931abb797e74dba2a189839aeb0de6d85150932f
Muhammad Zaid Dildar <142285065+Zaid-Dildar@users.noreply.github.com>
2024-11-06 23:11:08 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log("height updated");
+function isMobileScreen() {
+return window.innerWidth <= 768; // Adjusts breakpoint for mobile
+}
+
+function setIframeHeight() {
+const iframe = document.getElementById("iframe");
+iframe.style.height = isMobileScreen() ? "2300px" : "1600px";
+}
+setIframeHeight();
+
+window.addEventListener("message", function (event) {
+if (event.data.type === "resize-iframe") {
+document.getElementById("iframe").style.height =
+event.data.height[isMobileScreen() ? 1 : 0];
+console.log("height updated in hostinger script");
+window.scrollTo({
+top: 50,
+behavior: "smooth",
+});
+}
+});
+
+window.addEventListener("resize", setIframeHeight);
