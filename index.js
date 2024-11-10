e3293918a64652cbf1715d0f3c7f9a49adba4b23
Muhammad Zaid Dildar <142285065+Zaid-Dildar@users.noreply.github.com>
2024-11-10 17:38:22 +0500

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ window.addEventListener("message", function (event) {
     iframe.style.height = `${event.data.height[isMobileScreen() ? 1 : 0]}px`;
     console.log("Height updated from message");
     // Trigger the height change
-    sendHeightToHostingerSection(`${isMobileScreen()?event.data.height[1]+50:event.data.height[0]}`); // Adjust based on your needs
+    sendHeightToHostingerSection(`${isMobileScreen()?event.data.height[1]+50:event.data.height[0]}px`); // Adjust based on your needs
     // Set flag to prevent immediate height reset
     heightSetByMessage = true;
     resetHeightSetByMessageFlag();
